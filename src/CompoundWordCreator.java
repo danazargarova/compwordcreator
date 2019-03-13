a301c9e07105990a0ca99554c76b12d6f64138c0
BT_1N3_06@BKTECH.LOCAL <BT_1N3_06@BKTECH.LOCAL>
2019-03-13 14:27:23 -0400

    Initial commit

---
+++ src/CompoundWordCreator.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class CompoundWordCreator {
+    private List<String> wordList;
+
+    private boolean inDictionary(String word){
+
+    }
+
+    private int findMaxLength(){
+        int temp=0;
+        for(int i =0; i<this.wordList.size();i++){
+            if(this.wordList.get(i).length()>temp){
+                temp=this.wordList.get(i).length();
+            }
+        }
+        return temp;
+    }
+
+    private void addCompoundWords(List<String>compoundWords, int letterSum){
+        
+    }
+}
